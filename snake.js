15a9ed43672b947101e13951b717e38cc237f6d8
CVDash <danceknife@gmail.com>
2026-09-09 10:45:48 +0200

    commit 1 9/09

---
+++ snake.js
@@ -0,0 +1,7 @@
+function hover(snakehover) {
+  snakehover.setAttribute('src', 'img/spil/snake2.png');
+}
+
+function unhover(snakehover) {
+  snakehover.setAttribute('src', 'img/spil/snake1.png');
+}
